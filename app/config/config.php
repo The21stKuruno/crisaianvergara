92637f0f7611b993b50fc3853efee93752767901
Cris Aian Vergara <The21stKuruno@yahoo.com>
2018-09-27 07:42:53 +0900

    workflow done

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'crisaianvergara');
+
+  // App Root
+  define('APPROOT', dirname(__FILE__, 2)) ;
+  // URL Root
+  define('URLROOT', 'http://localhost/crisaianvergara');
+  // Site Namme
+  define('SITENAME', 'Cris Aian Vergara');
+  // App Version
+  define('APPVERSION', '2.0.0');
